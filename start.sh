52a77122f153a81f6519447551090d0f3d15084d
pawan <pawanbhakuni1290@gmail.com>
2025-02-07 17:40:52 +0530

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,61 @@
 #!/bin/bash
 
 module_name="etl_edr_p4"
-main_module="etl_module/etl_edr" #keep it empty "" if there is no main module 
+main_module="etl_edr" #keep it empty "" if there is no main module 
 log_level="INFO" # INFO, DEBUG, ERROR
+
+########### DO NOT CHANGE ANY CODE OR TEXT AFTER THIS LINE #########
+
 
 . ~/.bash_profile
 
-set -x
-VAR=""
 build="${module_name}.jar"
-cd ${APP_HOME}/${main_module}/${module_name}
-log_path="${LOG_HOME}/${main_module}"
 
-status=`ps -ef | grep $build | grep java`
-if [ "$status" != "$VAR" ]
+pid=`ps -ef | grep $build | grep java | grep $module_name | grep -v grep | awk '{print $2}'`
+
+if [ "${pid}" != "" ]  ## Process is currently running
 then
- echo "The process is already running"
- echo $status
-else
- echo "Starting process"
-  java -Dlog4j.configurationFile=./log4j2.xml -Dmodule.name=${module_name} -Dspring.config.location=file:${commonConfigurationFile},file:./application.properties -jar ${build} $(date '+%Y-%m-%d') 1>/dev/null 2>${log_path}/${module_name}.error &
+  echo "${module_name} process is currently running with PID ${pid} ..."
 
-# java -Dmodule.name=ETL_EDR_P4 -Xmx1024m -Xms256m -Dlog4j.configurationFile=./log4j2.xml -Dspring.config.location=file:${commonConfigurationFile},file:./application.properties -jar $PNAME $current_date 1>/u02/eirs 2>error.txt &
- echo "Process started"
+else  ## No process running
+
+  if [ "${main_module}" == "" ]
+  then
+     build_path="${APP_HOME}/${module_name}_module"
+     log_path="${LOG_HOME}/${module_name}_module"
+  else
+     if [ "${main_module}" == "utility" ] || [ "${main_module}" == "api_service" ] || [ "${main_module}" == "gui" ]
+     then
+       build_path="${APP_HOME}/${main_module}/${module_name}"
+       log_path="${LOG_HOME}/${main_module}/${module_name}"
+     else
+       build_path="${APP_HOME}/${main_module}_module/${module_name}"
+       log_path="${LOG_HOME}/${main_module}_module/${module_name}"
+     fi
+  fi
+
+  cd ${build_path}
+
+  ## Starting the process
+
+  echo "Starting ${module_name} module..."
+
+#   java -Xmx1024m -Xms256m -Dlog4j.configurationFile=./log4j2.xml -Dspring.config.location=file:./application.yml -jar $PNAME REMINDER_UTILITY duplicate
+
+  java -Dlog4j.configurationFile=./log4j2.xml -Dlog.level=${log_level} -Dlog.path=${log_path} -Dmodule.name=${module_name} -Dspring.config.location=file:./application.properties,file:${commonConfigurationFile} -jar ${build} $(date '+%Y-%m-%d') 1>/dev/null 2>${log_path}/${module_name}.error &
+
+  ## check if process started successfully or not
+
+  pid=`ps -ef | grep $build | grep java | grep $module_name | grep -v grep | awk '{print $2}'`
+  if [ "$pid" == "" ]
+  then
+
+    echo "Failed to start $module_name process !!!"
+
+  else
+    
+    echo "$module_name process is started successfully with PID ${pid} ..."
+
+  fi
+  
 fi
